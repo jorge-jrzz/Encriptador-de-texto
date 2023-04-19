77e6201fd025d0ad9463046fde877a7485929761
jorge Juarez <jorgeang33@gmail.com>
2023-04-19 12:39:02 -0600

    Funcion de desencriptar texto

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function encriptar() {
 
   // Verifica si la cadena no esta vacia
   if (cadena != "") {
-    // Oculta el mensaje y muestar el area de texto done estara la respuesta
+    // Oculta el mensaje y muestra el area de texto donde estara la respuesta
     output.style.display = "block";
     mensaje_temp.style.display = "none";
 
@@ -17,7 +17,7 @@ function encriptar() {
       const letra = cadena[i];
       let change = false;
 
-      // Verifica que el carracter no sea un espacio
+      // Verifica que el caracter no sea un espacio
       if (letra != " ") {
         // Recorre el arreglo de las vocales para saber si el caracter es una vocal
         for (let j = 0; j < vocales.length; j++) {
@@ -41,7 +41,60 @@ function encriptar() {
     output.value = text_encriptado;
     botton_copiar.style.display = "block";
 
-    // En caso de que la cadena este vacia, se muestar el mensaje correspondiente
+    // En caso de que la cadena este vacia, se muestra el mensaje correspondiente
+  } else {
+    output.style.display = "none";
+    botton_copiar.style.display = "none";
+    mensaje_temp.style.display = "block";
+  }
+}
+
+function desencriptar() {
+  // Obtiene el texto de entrada para ser desencriptado
+  cadena = textarea.value;
+  let text_desencriptado = "";
+
+  // Verifica si la cadena no esta vacia
+  if (cadena != "") {
+    // Oculta el mensaje y muestra el area de texto donde estara la respuesta
+    output.style.display = "block";
+    mensaje_temp.style.display = "none";
+
+    // Recorre la cadena letra por letra para encontrar las vocales
+    for (let i = 0; i < cadena.length; i++) {
+      const letra = cadena[i];
+      let change = false;
+
+      // Verifica que el caracter no sea un espacio
+      if (letra != " ") {
+        // Recorre el arreglo de las vocales para saber si el caracter es una vocal
+        for (let j = 0; j < vocales.length; j++) {
+          const vocal = vocales[j];
+
+          // Si el caracter es una vocal, se recorre el arreglo de los cambios, y como los dos arreglos
+          // (vocales y cambios) tiene el mismo orden y tamaño ya tambien es conoicida el cambio correspondiente
+          // a los caracteres siguientes en la cadena; por lo que al mismo tiempo se incrementa i (contador de los
+          // caracteres de la cadena original) para omitir de cierta forma estos caracteres.
+          // Finalmente se intercambia por su correspondiente en el texto resultado y sale de la busqueda
+          if (letra == vocal) {
+            for (let k = 0; k < cambios[j].length; k++, i++) {}
+            i--;
+            text_desencriptado += vocales[j];
+            change = true;
+            break;
+          }
+        }
+      }
+
+      // Si el caracter no es vocal, se agrega al texto resultado
+      if (!change) text_desencriptado += letra;
+    }
+
+    // Se muestra en resultado en pantalla y se muestra el boton de copiar
+    output.value = text_desencriptado;
+    botton_copiar.style.display = "block";
+
+    // En caso de que la cadena este vacia, se muestra el mensaje correspondiente
   } else {
     output.style.display = "none";
     botton_copiar.style.display = "none";
